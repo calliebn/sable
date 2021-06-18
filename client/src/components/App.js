b1b16c142ae29a7bbd5526bdd32c22650b72b8e8
Kp <kp_1@outlook.com>
2021-06-18 21:07:59 +0400

    Dashboard updates

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Login from './Login';
 import useLocalStorage from '../hooks/useLocalStorage';
+import Dashboard from './Dashboard';
 
 function App() {
   // save in local storge in 'Application'
   const [id, setId] = useLocalStorage('id');
+
+  // If you have id, do not need login page
+  // If we have an Id, go to another page. If not, use login page
+  //  Or got to Dashboard component & pass id
   return (
+    //!!!! CHECK IF WE NEED THE FRAGMENTS OR NOT
     <>
-      {id}j
-      <Login onIdSubmit={setId} />;
+      id ? : <Dashboard id={id} /> : <Login onIdSubmit={setId} />
     </>
   );
 }
