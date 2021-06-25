296958876d29677a3d7637e7bb16416e20778d38
Kp <kp_1@outlook.com>
2021-06-25 13:37:39 +0400

    Udpaes to Chats provider

--- client/src/contexts/ChatsProvider.js
+++ client/src/contexts/ChatsProvider.js
@@ -8,8 +8,8 @@ export function useChats() {
   return useContext(ChatsContext);
 }
 
-// THIS IS NOT WORKING!
-export function ChatsProvider({ children }) {
+// pass id as prop into convo
+export function ChatsProvider({ id, children }) {
   const [chats, setChats] = useLocalStorage('chats', []);
 
   // Create a state with the first chat
@@ -22,6 +22,46 @@ export function ChatsProvider({ children }) {
     setChats((prevChats) => {
       return [...prevChats, { recipients, messages: [] }];
     });
+  }
+
+  // Parameters: Reciver, text, sender
+  // msg that gets called from both server (send a msg)
+  // and send msg to other people
+  function addMessageToChat({ recipients, text, sender }) {
+    setChats((prevChats) => {
+      // convo that matches the recipent.
+      // create new message
+
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newChats = prevChats.map((chat) => {
+        // If recipient array matches the recipients of each
+        // individual convo
+        if (arrayEquality(chat.recipients, recipients)) {
+          madeChange = true;
+          //  new chat
+          return { ...chat, messages: [...chat.messages, newMessage] };
+        }
+        //  if not true, return convo
+        return chat;
+      });
+
+      // Loop for changes and no changes
+      if (madeChange) {
+        return newChats;
+      } else {
+        return [
+          // Taking current chats
+          ...prevChats,
+          // Adding new Chats
+          { recipients, messages: [newMessage] },
+        ];
+      }
+    });
+  }
+
+  function sendMessage(recipients, text) {
+    addMessageToChat({ recipients, text, sender: id });
   }
 
   // Store name of recipients
@@ -39,6 +79,9 @@ export function ChatsProvider({ children }) {
       return { id: recipient, name };
     });
     // To figure if chat selected. Determining if we get index from above
+    
+    cosnt messages = 
+    
     const selected = index === selectedChatIndex;
     // new object that has everthing about our chats
     // replacing recipients with the new formatted text
@@ -51,6 +94,7 @@ export function ChatsProvider({ children }) {
     createChat,
     // Use selected convo at a later point
     selectedChat: formattedChats[selectedChatIndex],
+    sendMessage,
     // mapping to a different name
     selectChatIndex: setSelectedChatIndex,
     createChat,
@@ -59,4 +103,18 @@ export function ChatsProvider({ children }) {
   return (
     <ChatsContext.Provider value={value}>{children}</ChatsContext.Provider>
   );
+}
+// Doesn't depend on anything in this component
+// take in 2 arrays
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  // otherwise, sort
+  a.sort();
+  b.sort();
+// every element in a is = every element in b at the same index position
+// arrays are exactyl equal
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
