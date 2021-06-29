4f80733a1631784f0fd9e694cf3733f373fa9a76
Kp <kp_1@outlook.com>
2021-06-29 05:54:42 +0400

    Text component

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import onlineIcon from '../../icons/onlineIcon.png';
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className='textContainer'>
+    <div>
+      <h1>
+        Welcome to this room!
+        <span role='img' aria-label='emoji'>
+          💬
+        </span>
+      </h1>
+      <h2>Click the little x on the top of the text box to go to home! </h2>
+    </div>
+    {users ? (
+      <div>
+        <h1>People currently chatting in this room:</h1>
+        <div className='activeContainer'>
+          <h2>
+            {users.map(({ name }) => (
+              <div key={name} className='activeItem'>
+                {name}
+                <img alt='Online Icon' src={onlineIcon} />
+              </div>
+            ))}
+          </h2>
+        </div>
+      </div>
+    ) : null}
+  </div>
+);
+
+export default TextContainer;
