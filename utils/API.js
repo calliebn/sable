6fa57ba54d79692e45349b4baed69e5c44e4f4e9
Kp <kp_1@outlook.com>
2021-07-04 06:10:59 +0400

    API

---
+++ utils/API.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default {
+  // Creates a user
+  saveUser: function () {
+    return axios.post('/api/user', userData);
+  },
+
+  // Gets the user with a given id
+  getUser: function (id) {
+    return axios.get('/api/user/' + id);
+  },
+
+  // Deletes the user by their id
+  deleteUser: function (id) {
+    return axios.delete('/api/user/' + id);
+  },
+};
