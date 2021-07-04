c31736911372a10ed07dbaa7bfc4db6b60fea4c7
Kp <kp_1@outlook.com>
2021-07-04 20:55:56 +0400

    Tweaks

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,34 +1,30 @@
-import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
-import { Link } from "react-router-dom";
-import { FcCloseUpMode } from "react-icons/fc";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { Button } from "../Button";
-import { IconContext } from "react-icons/lib";
-import Yarn from '../../pages/Yarn'
-import Home from "../Home";
-import Profile from "../Profile";
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import { Link } from 'react-router-dom';
+import { FcCloseUpMode } from 'react-icons/fc';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Button } from '../Button';
+import { IconContext } from 'react-icons/lib';
+import Yarn from '../../pages/Yarn';
+import Home from '../Home';
+import Profile from '../Profile';
 
-import AuthService from "../../services/auth.service";
-
+import AuthService from '../../services/auth.service';
 
 const Navbar = () => {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
 
-const [currentUser, setCurrentUser] =useState(undefined);
-useEffect(() => {
-  const user =AuthService.getCurrentUser();
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
 
-  if(user) {
-    setCurrentUser(user)
-  }
-}, []);
-
-const logOut = () => {
-  AuthService.logout();
-}
-
-
+  const logOut = () => {
+    AuthService.logout();
+  };
 
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
@@ -46,98 +42,100 @@ const logOut = () => {
     showButton();
   }, []);
 
-  window.addEventListener("resize", showButton);
+  window.addEventListener('resize', showButton);
   return (
     <>
-
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <div className="navbar">
-          <div className="navbar-container container">
-            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-              <FcCloseUpMode className="navbar-icon" />
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <FcCloseUpMode className='navbar-icon' />
               Sable
             </Link>
-            <div className="menu-icon" onClick={handleClick}>
+            <div className='menu-icon' onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link  to={'/'} className ='nav-links'>Home
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to={'/'} className='nav-links'>
+                  Home
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className='nav-item'>
                 <Link
-                  to="/yarn"
-                  className="nav-links"
+                  to='/yarn'
+                  className='nav-links'
                   onClick={closeMobileMenu}
                 >
                   <Yarn />
                   Yarn
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className='nav-item'>
                 <Link
-                  to="/message"
-                  className="nav-links"
+                  to='/message'
+                  className='nav-links'
                   onClick={closeMobileMenu}
                 >
                   Message
                 </Link>
               </li>
+              <li className='nav-item'>
+                <Link
+                  to='/chat'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Chat
+                </Link>
+              </li>
 
-{currentUser && (
-  
-    <li className = 'nav-item'>
-      <Link to={'/user'} className='nav-links'>User</Link>
-    </li>
-  
-)}
+              {currentUser && (
+                <li className='nav-item'>
+                  <Link to={'/user'} className='nav-links'>
+                    User
+                  </Link>
+                </li>
+              )}
 
-{currentUser ? (
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/profile"} className="nav-links">
-                {currentUser.username}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a href="/login" className="nav-links" onClick={logOut}>
-                LogOut
-              </a>
-            </li>
-          </div>
-) : (
-  <div className="navbar-nav ml-auto">
-  <li className="nav-item">
-    <Link to={"/login"} className="nav-links">
-      Login
-    </Link>
-  </li>
+              {currentUser ? (
+                <div className='navbar-nav ml-auto'>
+                  <li className='nav-item'>
+                    <Link to={'/profile'} className='nav-links'>
+                      {currentUser.username}
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <a href='/login' className='nav-links' onClick={logOut}>
+                      LogOut
+                    </a>
+                  </li>
+                </div>
+              ) : (
+                <div className='navbar-nav ml-auto'>
+                  <li className='nav-item'>
+                    <Link to={'/login'} className='nav-links'>
+                      Login
+                    </Link>
+                  </li>
+                </div>
+              )}
 
-</div>
-)}
-
-
-
-              <li className="nav-btn">
+              <li className='nav-btn'>
                 {button ? (
-                  <Link to={'/register'} className="btn-link">
-                    <Button buttonStyle="btn--outline">Sign Up</Button>
+                  <Link to={'/register'} className='btn-link'>
+                    <Button buttonStyle='btn--outline'>Sign Up</Button>
                   </Link>
                 ) : (
                   <Link
-                    to="/register"
-                    className="btn-link"
+                    to='/register'
+                    className='btn-link'
                     onClick={closeMobileMenu}
                   >
-                    <Button className="btn--outline" buttonSize="btn--mobile">
-                      Sign Up 
+                    <Button className='btn--outline' buttonSize='btn--mobile'>
+                      Sign Up
                     </Button>
-                   
-               
-                  
                   </Link>
-                  
                 )}
               </li>
             </ul>
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import { BrowserRouter } from "react-router-dom";
-=======
->>>>>>> fc85ebd716f0fb9f10656cdec1f09adc6dbb53ad
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-<<<<<<< HEAD
-ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-
-=======
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> fc85ebd716f0fb9f10656cdec1f09adc6dbb53ad
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
